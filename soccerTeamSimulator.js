b63443eadabdcc0e5664494d9e4f4f1f7bddea98
sanchezdamianj <sanchezdamianj@gmail.com>
2022-06-13 15:20:08 -0300

    responsive desing and enhacements in functions

--- soccerTeamSimulator.js
+++ soccerTeamSimulator.js
@@ -14,19 +14,20 @@ createInitialList.addEventListener("submit", ()=> {
 });
 
 const addInitialList = () => {    
-        playersList.push(new Player('emiliano' ,'martinez', 8, 'Portero'));
-        playersList.push(new Player('cristian' ,'romero',9,'Defensa central'));
-        playersList.push(new Player('nicolas' ,'otamendi',9,'Defensa central'));
-        playersList.push(new Player('marcos','acunia', 7,'Lateral izquierdo' ));   
-        playersList.push(new Player('gonzalo','montiel',9, 'Lateral Derecho'));
-        playersList.push(new Player('leandro','paredes',9,'Pivote'));
-        playersList.push(new Player('rodrigo','de paul',10, 'Mediocentro'));
-        playersList.push(new Player('giovani','lo celso',10, 'Mediocentro'));   
-        playersList.push(new Player('lautaro','martinez',9, 'Delantero centro'));
-        playersList.push(new Player('angel','di maria',10, 'Extremo Derecho'));
-        playersList.push(new Player('lionel','messi',10, 'Todas'));  
+        playersList.push(new Player('emiliano' ,'martinez', 10, 'Goalkeeper'));
+        playersList.push(new Player('cristian' ,'romero',10,'Defence'));
+        playersList.push(new Player('nicolas' ,'otamendi',10,'Defence'));
+        playersList.push(new Player('marcos','acunia', 10,'Winger' ));   
+        playersList.push(new Player('gonzalo','montiel',10, 'Winger'));
+        playersList.push(new Player('leandro','paredes',10,'Middle'));
+        playersList.push(new Player('rodrigo','de paul',10, 'Middle'));
+        playersList.push(new Player('giovani','lo celso',10, 'Middle'));   
+        playersList.push(new Player('lautaro','martinez',10, 'Forward'));
+        playersList.push(new Player('angel','di maria',10, 'Forward'));
+        playersList.push(new Player('lionel','messi',10, 'All'));  
         alert("You created the initial players list");
-        showInitList(playersList);        
+        showInitList(playersList);   
+        rankTotal(playersList);     
 }
 
 function welcome() {
@@ -52,7 +53,7 @@ function validateUser(user, password) {
         
 
 }
-welcome();
+// welcome();
 
 const form = document.getElementById("eventForm");
 form.addEventListener("submit", function(event) {
@@ -72,11 +73,12 @@ const returnPlayers = async ()=>{
     let i = players.length - 1;
 	player[i] = await getPlayer(i);
 	let newHTMLCode = `
-		<div class="player">
-		    <div class="name">${player[i].name}${player[i].surname}</div>
-		    <div class="qualification">${player[i].qualification}</div> 
-            <div class="position">${player[i].position}</div> 
-		 </div>`;
+		<tr>
+            <th>${player[i].qualification}</th>
+		    <td class="name">${player[i].name} ${player[i].surname}</td>
+		    <td class="qualification">${player[i].qualification}</td> 
+            <td class="position">${player[i].position}</td> 
+		 </tr>`;
 	playersHTML.innerHTML += newHTMLCode;  
     localStorage.setItem(player[i].name, player[i].qualification, player[i].position);   
     form.reset();
@@ -87,12 +89,12 @@ function addPlayer(){
     let surnameToBeAdded = document.getElementById("surnamePlayer").value
     let qualificationNew = parseInt(document.getElementById("qualificationNew").value);   
     let position = document.getElementById("position").value;   
-    let playerNew = new Player(nameToBeAdded,surnameToBeAdded, qualificationNew ,position);
+    let playerNew = new Player(nameToBeAdded,surnameToBeAdded,qualificationNew ,position);
     if(players = [{}]){
         players = [...playersList];
     }
     if (namePlayer.value !== "Enter a player"){
-        let pos = players.findIndex(element => element.name === surnameToBeAdded);
+        let pos = players.findIndex(element =>  element.name === nameToBeAdded || element.name === surnameToBeAdded);
         if (pos >= 0){   
             players.splice(pos, 1)};
             players.push(playerNew);
@@ -108,8 +110,9 @@ function delPlayer(){
       };  
 }
 
-const rankingTotalHTML = document.querySelector(".total");
+
 const rankTotal = (players) =>{
+    const rankingTotalHTML = document.querySelector(".total");
     let sum = players.map(player => player.qualification).reduce((prev, curr) => prev + curr, 0);
     if(sum){
     let rankHTMLCode = `<div class="total">${sum}</div>`;
@@ -118,52 +121,47 @@ const rankTotal = (players) =>{
     return sum;
 }
 
+const htmlStructure = (imgPath,arrPlayers, index) =>{
+    const galleryHTML = document.querySelector(".gallery");
+     let newGalleryHTMLCode = `      
+        <div class="card" style="width: 225px; height:375px;">
+            <img src="${imgPath}" class="card-img-top" alt="${arrPlayers[index].name}">
+            <div class="card-body">
+                <h5 class="card-title">${arrPlayers[index].name} ${arrPlayers[index].surname}</h5>
+                <p class="card-text">${arrPlayers[index].qualification}</p>
+                <a href="#" class="btn btn-primary">Add to Team</a>
+            </div>
+        </div>`;
+    galleryHTML.innerHTML += newGalleryHTMLCode;
+}
+
 function showInitList(setPlayers) {
   let i;
-  let namePic = "";
-  let completeName = "";
-  let surnamePic = "";
+  let namePic,completeName,surnamePic = "";
   for (i = 0; i < setPlayers.length; i++) {
     namePic = setPlayers[i].name;
     surnamePic = setPlayers[i].surname;
     completeName = (("./assets/"+namePic+" "+surnamePic+".jpg").replace(/[' "]+/g, ' ')).toLowerCase();
-    const galleryHTML = document.querySelector(".gallery");
-    if(completeName){
-        let newGalleryHTMLCode = `
-             <div class="gallery">
-                <div>   
-                    <img src="${completeName}"/>
-                    <p class="qualy">Qualification: ${setPlayers[i].qualification}</p>
-                </div>
-             </div>`;
-        galleryHTML.innerHTML += newGalleryHTMLCode;
-    }
+    completeName?htmlStructure(completeName, setPlayers, i):console.log("There is no player with this name");
   }
 }
 
-function searchPlayers() {
-  let i;
+function searchPlayers(){
+  let i,posRes= 0;
   let res = [...playersList];
-  let namePic = "";
-  let completeName = "";
+  let namePic,completeName = "";
   let input = document.getElementById("search").value;
-  for (i = 0; i < res.length; i++) {
+  for (i; i < res.length; i++) {
     if (input === res[i].surname || input === res[i].name){
        namePic = res[i].name;
+       posRes = i;
        completeName = (("./assets/"+namePic+" "+input+".jpg").replace(/[' "]+/g, ' ')).toLowerCase();
     }
   }
-  const galleryHTML = document.querySelector(".gallery");
-    if(completeName){
-  let newGalleryHTMLCode = `
-             <div class="gallery">
-              <img src="${completeName}"/>
-             </div>`;
-  galleryHTML.innerHTML += newGalleryHTMLCode;
-    }
+  completeName?htmlStructure(completeName, res, posRes): alert("There is no player with this name");   
 }
 
-document.getElementById("btn-searchPlayers").addEventListener("click", (e)=>{
+document.getElementById("btn-searchPlayers").addEventListener("click",(e)=> {
     e.preventDefault();
 });
 
